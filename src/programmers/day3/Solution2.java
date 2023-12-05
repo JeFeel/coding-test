b3ec77ba03f8f98f9b9109eb3c8f5f6e05afebc4
JeFeel <jaephil94@gmail.com>
2023-12-06 01:33:41 +0900

    Add : Day 3 Solution2 리스트 문자열 변환

---
+++ src/programmers/day3/Solution2.java
@@ -0,0 +1,14 @@
+package programmers.day3;
+
+
+public class Solution2 {
+
+    public String solution(String[] arr) {
+        String answer = "";
+
+        for (int i = 0; i < arr.length; i++) {
+            answer += arr[i];
+        }
+        return answer;
+    }
+}
