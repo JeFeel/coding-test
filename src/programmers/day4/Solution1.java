ef14a0a071db3f627a1769f6b32740bc67a4736a
JeFeel <jaephil94@gmail.com>
2023-12-07 10:29:46 +0900

    Add : Day 4 Solution1 n의 배수

---
+++ src/programmers/day4/Solution1.java
@@ -0,0 +1,14 @@
+package programmers.day4;
+
+
+public class Solution1 {
+
+    public int solution(int num, int n) {
+        int answer = 0;
+
+        if (num % n == 0) {
+            answer = 1;
+        }
+        return answer;
+    }
+}
