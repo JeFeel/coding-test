e66b3c64240cb6ba7c6a26c96a648c62130f0e48
JeFeel <jaephil94@gmail.com>
2023-12-08 22:02:09 +0900

    Add : Day 5 Solution4 원소들의 곱과 합

---
+++ src/programmers/day5/Solution4.java
@@ -0,0 +1,22 @@
+package programmers.day5;
+
+
+public class Solution4 {
+
+    public int solution(int[] num_list) {
+//        int answer = 0;
+        int multiply = 1;
+        int sum = 0;
+
+        for (int i : num_list) {
+            multiply *= i;
+            sum += i;
+        }
+
+        int squareSum = (int) Math.pow(sum,2);
+
+//        if (multiply<squareSum) answer = 1;
+
+        return multiply<squareSum ? 1 : 0;
+    }
+}
