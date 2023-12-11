2aa9703b15b5a281001dfbd3b4b9213c9e4b5a11
JeFeel <jaephil94@gmail.com>
2023-12-11 15:42:54 +0900

    Add : Day7 Solution3 카운트 업

---
+++ src/programmers/week1/day7/Solution3.java
@@ -0,0 +1,12 @@
+package programmers.week1.day7;
+
+
+import java.util.stream.IntStream;
+
+
+public class Solution3 {
+    public int[] solution(int start_num, int end_num) {
+
+        return IntStream.rangeClosed(start_num, end_num).toArray();
+    }
+}
