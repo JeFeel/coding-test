2e78307089fed83545d3dcdb2eaf79ccd8bb7294
JeFeel <jaephil94@gmail.com>
2023-12-13 22:35:42 +0900

    Add : Day9 Solution3 문자열 뒤의 n글자

---
+++ src/programmers/week2/day9/Solution3.java
@@ -0,0 +1,10 @@
+package programmers.week2.day9;
+
+
+public class Solution3 {
+
+    public String solution(String my_string, int n) {
+        return my_string.substring(my_string.length()-n);
+    }
+
+}
