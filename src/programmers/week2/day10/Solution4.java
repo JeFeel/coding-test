d51e798c03851ea0f65669054d3a859fba362135
JeFeel <jaephil94@gmail.com>
2023-12-14 21:51:05 +0900

    Add : Day10 Solution4 세로 읽기

---
+++ src/programmers/week2/day10/Solution4.java
@@ -0,0 +1,22 @@
+package programmers.week2.day10;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Solution4 {
+    public String solution(String my_string, int m, int c) {
+        StringBuilder answer = new StringBuilder();
+        List<String> sliceStr = new ArrayList<>();
+
+        for (int i = 0; i < my_string.length(); i+=m) {
+            sliceStr.add(my_string.substring(i, i+m));
+        }
+
+        for (String string : sliceStr) {
+            answer.append(string.charAt(c-1));
+        }
+        return answer.toString();
+    }
+}
