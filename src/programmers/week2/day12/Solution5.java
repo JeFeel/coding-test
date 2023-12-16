0d2015c4b92df1816e667a95588f596a178a787c
JeFeel <jaephil94@gmail.com>
2023-12-16 17:03:49 +0900

    Refactor : Day12 Solution5 배열 조각하기, Stream 풀이 추가

--- src/programmers/week2/day12/Solution5.java
+++ src/programmers/week2/day12/Solution5.java
@@ -4,6 +4,8 @@ package programmers.week2.day12;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 
 public class Solution5 {
     public int[] solution(int[] arr, int[] query) {
@@ -18,5 +20,13 @@ public class Solution5 {
             }
         }
         return list.stream().mapToInt(i->i).toArray();
+
+        // Stream으로 풀어보기
+//        return IntStream.range(0, query.length)
+//                .boxed() // IntStream에서 Stream<Integer> 객체로
+//                .reduce(arr, (current, next) -> next % 2 == 0 ?
+//                                Arrays.copyOfRange(current, 0, query[next] + 1) :
+//                                Arrays.copyOfRange(current, query[next], current.length),
+//                        (a,b)->a);
     }
 }
